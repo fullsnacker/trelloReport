9777aae1cb3b1b7fc913dd76a67ac0ad9a8d25af
fullsnacker <fullsnacker@gmail.com>
2025-03-21 14:44:12 -0300

    Added reprogram and assign

--- src/reportFile.js
+++ src/reportFile.js
@@ -3,6 +3,9 @@ const getLast30DaysActions = require("./utils/getLast30DaysActions");
 const getDataFromActions = require("./utils/getDataFromActions");
 const writeToFile = require("./utils/writeToFile");
 
+//const initialDate = new Date("2025-03-21 00:00:00");
+const initialDate = new Date("2025-01-01 00:00:00");
+
 const reportFile = (filePath) => {
   fs.readFile(filePath, "utf8", (err, data) => {
     if (err) {
@@ -13,8 +16,12 @@ const reportFile = (filePath) => {
     try {
       const jsonData = JSON.parse(data);
       const allActions = jsonData.actions;
-      const actionsLast30Days = getLast30DaysActions(allActions);
-      const result = getDataFromActions(actionsLast30Days);
+      // const actionsLast30Days = getLast30DaysActions(allActions);
+      const actionsSinceInit = allActions.filter(
+        (action) => new Date(action.date) > initialDate
+      );
+      const result = getDataFromActions(actionsSinceInit);
+      console.log(result);
       writeToFile(result);
     } catch (parseErr) {
       console.error("Error parsing JSON:", parseErr.message);
--- src/utils/getDataFromActions.js
+++ src/utils/getDataFromActions.js
@@ -1,14 +1,40 @@
 const getDataFromActions = (actions) => {
   let result = "Date,Time,Card,Type,Description,Elapsed Time\n";
   actions.map((action) => {
-    if (action.type === "updateCard" && action.data.listBefore) {
-      result += `${new Date(action.date).toLocaleDateString(
-        "es-ES"
-      )},${new Date(action.date).toLocaleTimeString("es-ES")},${
-        action.data.card.name
-      },changeList,From '${action.data.listBefore.name}' To '${
-        action.data.listAfter.name
-      }'\n`;
+    if (action.type === "updateCard") {
+      if (action.data.listBefore) {
+        result += `${new Date(action.date).toLocaleDateString(
+          "es-ES"
+        )},${new Date(action.date).toLocaleTimeString("es-ES")},${
+          action.data.card.name
+        },changeList,From '${action.data.listBefore.name}' To '${
+          action.data.listAfter.name
+        }'\n`;
+      }
+      if (action.data.old?.due) {
+        result += `${new Date(action.date).toLocaleDateString(
+          "es-ES"
+        )},${new Date(action.date).toLocaleTimeString("es-ES")},${
+          action.data.card.name
+        },reprogram,From '${new Date(action.data.old.due).toLocaleDateString(
+          "es-ES"
+        )} ${new Date(action.data.old.due).toLocaleTimeString(
+          "es-ES"
+        )}' To '${new Date(action.data.card.due).toLocaleDateString(
+          "es-ES"
+        )} ${new Date(action.data.card.due).toLocaleTimeString("es-ES")}'\n`;
+      } else if (
+        action.data.old?.due === null &&
+        action.data.card.due != null
+      ) {
+        result += `${new Date(action.date).toLocaleDateString(
+          "es-ES"
+        )},${new Date(action.date).toLocaleTimeString("es-ES")},${
+          action.data.card.name
+        },assignDate\,Date: ${new Date(action.data.card.due).toLocaleDateString(
+          "es-ES"
+        )} ${new Date(action.data.card.due).toLocaleTimeString("es-ES")}\n`;
+      }
     } else if (action.type === "commentCard") {
       result += `${new Date(action.date).toLocaleDateString(
         "es-ES"
